26fb949950b08a501c4bee4b7f79719a86956dff
Jean-David Moisan <vynecompiler@gmail.com>
2019-05-06 20:01:21 -0400

    Added Core platform and standalone pipeline

---
+++ Platforms/Core/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using(var game = new Game1())
+            game.Run();
+        }
+    }
+}
---
+++ Platforms/Pipeline/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CommandLine;
+
+namespace Pipeline {
+    class Program {
+        static void Main(string[] args) {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+                Console.WriteLine("Input path: " + o.Input);
+                Console.WriteLine("Output path: " + o.Output);
+
+                Pipeline p = new Pipeline(o.Input, o.Output);
+            });
+        }
+
+        private class Options {
+            [Option('i', "input", Required = true, HelpText = "Sets content input path.")]
+            public string Input {
+                get;
+                set;
+            }
+            [Option('o', "output", Required = true, HelpText = "Sets content output path.")]
+            public string Output {
+                get;
+                set;
+            }
+        }
+    }
+}
