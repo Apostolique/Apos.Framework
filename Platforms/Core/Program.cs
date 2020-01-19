a4324ecacc2e0b86eb59d22fa822fd43a5604e9b
Jean-David Moisan <vynecompiler@gmail.com>
2020-01-19 18:12:42 -0500

    Upgrade to .NET core 3.1

--- Platforms/Core/Program.cs
+++ Platforms/Core/Program.cs
@@ -4,7 +4,7 @@ namespace GameProject {
     public static class Program {
         [STAThread]
         static void Main() {
-            using(var game = new GameRoot())
+            using var game = new GameRoot();
             game.Run();
         }
     }
