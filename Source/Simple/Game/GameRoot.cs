48a9f2aa6aea530ff29a0e6f72e211518ef7c7d1
Jean-David Moisan <vynecompiler@gmail.com>
2022-01-12 21:35:27 -0500

    Added better defaults, upgrade to .net6

--- Source/Simple/Game/GameRoot.cs
+++ Source/Simple/Game/GameRoot.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Apos.Input;
@@ -10,10 +13,23 @@ namespace GameProject {
             _graphics.GraphicsProfile = GraphicsProfile.HiDef;
             IsMouseVisible = true;
             Content.RootDirectory = "Content";
+
+            _settings = EnsureJson<Settings>("Settings.json");
         }
 
         protected override void Initialize() {
             // TODO: Add your initialization logic here
+            Window.AllowUserResizing = true;
+
+            IsFixedTimeStep = _settings.IsFixedTimeStep;
+            _graphics.SynchronizeWithVerticalRetrace = _settings.IsVSync;
+
+            _settings.IsFullscreen = _settings.IsFullscreen || _settings.IsBorderless;
+
+            RestoreWindow();
+            if (_settings.IsFullscreen) {
+                ApplyFullscreenChange(false);
+            }
 
             base.Initialize();
         }
@@ -25,11 +41,28 @@ namespace GameProject {
             InputHelper.Setup(this);
         }
 
+        protected override void UnloadContent() {
+            if (!_settings.IsFullscreen) {
+                SaveWindow();
+            }
+
+            SaveJson<Settings>("Settings.json", _settings);
+
+            base.UnloadContent();
+        }
+
         protected override void Update(GameTime gameTime) {
             InputHelper.UpdateSetup();
 
             if (_quit.Pressed())
                 Exit();
+
+            if (_toggleFullscreen.Pressed()) {
+                ToggleFullscreen();
+            }
+            if (_toggleBorderless.Pressed()) {
+                ToggleBorderless();
+            }
 
             // TODO: Add your update logic here
 
@@ -45,13 +78,121 @@ namespace GameProject {
             base.Draw(gameTime);
         }
 
+        private void ToggleFullscreen() {
+            bool oldIsFullscreen = _settings.IsFullscreen;
+
+            if (_settings.IsBorderless) {
+                _settings.IsBorderless = false;
+            } else {
+                _settings.IsFullscreen = !_settings.IsFullscreen;
+            }
+
+            ApplyFullscreenChange(oldIsFullscreen);
+        }
+        private void ToggleBorderless() {
+            bool oldIsFullscreen = _settings.IsFullscreen;
+
+            _settings.IsBorderless = !_settings.IsBorderless;
+            _settings.IsFullscreen = _settings.IsBorderless;
+
+            ApplyFullscreenChange(oldIsFullscreen);
+        }
+
+        public static string GetPath(string name) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, name);
+        public static T LoadJson<T>(string name) where T : new() {
+            T json;
+            string jsonPath = GetPath(name);
+
+            if (File.Exists(jsonPath)) {
+                json = JsonSerializer.Deserialize<T>(File.ReadAllText(jsonPath), _options);
+            } else {
+                json = new T();
+            }
+
+            return json;
+        }
+        public static void SaveJson<T>(string name, T json) {
+            string jsonPath = GetPath(name);
+            string jsonString = JsonSerializer.Serialize(json, _options);
+            File.WriteAllText(jsonPath, jsonString);
+        }
+        public static T EnsureJson<T>(string name) where T : new() {
+            T json;
+            string jsonPath = GetPath(name);
+
+            if (File.Exists(jsonPath)) {
+                json = JsonSerializer.Deserialize<T>(File.ReadAllText(jsonPath), _options);
+            } else {
+                json = new T();
+                string jsonString = JsonSerializer.Serialize(json, _options);
+                File.WriteAllText(jsonPath, jsonString);
+            }
+
+            return json;
+        }
+
+        private void ApplyFullscreenChange(bool oldIsFullscreen) {
+            if (_settings.IsFullscreen) {
+                if (oldIsFullscreen) {
+                    ApplyHardwareMode();
+                } else {
+                    SetFullscreen();
+                }
+            } else {
+                UnsetFullscreen();
+            }
+        }
+        private void ApplyHardwareMode() {
+            _graphics.HardwareModeSwitch = !_settings.IsBorderless;
+            _graphics.ApplyChanges();
+        }
+        private void SetFullscreen() {
+            SaveWindow();
+
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.HardwareModeSwitch = !_settings.IsBorderless;
+
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+        }
+        private void UnsetFullscreen() {
+            _graphics.IsFullScreen = false;
+            RestoreWindow();
+        }
+        private void SaveWindow() {
+            _settings.X = Window.ClientBounds.X;
+            _settings.Y = Window.ClientBounds.Y;
+            _settings.Width = Window.ClientBounds.Width;
+            _settings.Height = Window.ClientBounds.Height;
+        }
+        private void RestoreWindow() {
+            Window.Position = new Point(_settings.X, _settings.Y);
+            _graphics.PreferredBackBufferWidth = _settings.Width;
+            _graphics.PreferredBackBufferHeight = _settings.Height;
+            _graphics.ApplyChanges();
+        }
+
         GraphicsDeviceManager _graphics;
         SpriteBatch _s;
+
+        Settings _settings;
 
         ICondition _quit =
             new AnyCondition(
                 new KeyboardCondition(Keys.Escape),
                 new GamePadCondition(GamePadButton.Back, 0)
             );
+        ICondition _toggleFullscreen =
+            new AllCondition(
+                new KeyboardCondition(Keys.LeftAlt),
+                new KeyboardCondition(Keys.Enter)
+            );
+        ICondition _toggleBorderless = new KeyboardCondition(Keys.F11);
+
+        private static JsonSerializerOptions _options = new JsonSerializerOptions {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
     }
 }
